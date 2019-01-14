a34adcc9e512d8c20ac4f916328742cbdb701cf6
mikekaczmarski <mkaczman@gmail.com>
2019-01-14 13:37:04 -0700

    testing jwt

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM stocktraderdemo/testbase
 
 ENV DISPLAY ""
 
-COPY StockTrader.tar /root/StockTrader.tar
+COPY ST.tar /root/ST.tar
 COPY looptest.sh /root/looptest.sh
-RUN  cd /root &&  tar -xvf StockTrader.tar
+RUN  cd /root &&  tar -xvf ST.tar
 
 CMD  "sh" "/root/looptest.sh"
--- looptest.sh
+++ looptest.sh
@@ -10,6 +10,10 @@ LOOPS=100000
 
 while [ $LOOPS -gt 0 ]
 do
-  /opt/IBM/RationalIntegrationTester/RunTests -noHTTP -environment ubuntu -project /root/StockTrader/StockTrader.ghp -run Suite 
+  export JWT=$(curl -k -L -H "Authorization: Basic YWRtaW46YWRtaW4=" https://looper-service:9443/looper/jwt)
+
+  echo "JWT: " $JWT
+
+  /opt/IBM/RationalIntegrationTester/RunTests -noHTTP -environment IKS -environmentTags JWT -project /root/StockTrader/StockTrader.ghp -run Suite 
   sleep 30
 done
