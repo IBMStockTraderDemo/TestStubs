8cd06680c5f63f216bbe8236ec1a3b296f7cda5a
Your Name <you@example.com>
2019-04-15 02:28:02 +0000

    updated for V9.5

--- looptest.sh
+++ looptest.sh
@@ -10,11 +10,11 @@ LOOPS=100000
 
 while [ $LOOPS -gt 0 ]
 do
-  JWT=$(curl -k -L -H "Authorization: Basic YWRtaW46YWRtaW4=" https://looper-service:9443/looper/jwt)
+  #JWT=$(curl -k -L -H "Authorization: Basic YWRtaW46YWRtaW4=" https://looper-service:9443/looper/jwt)
 
-  echo "JWT: " $JWT
+  #echo "JWT: " $JWT
 
-  export JWT
+  #export JWT
 
   /opt/IBM/RationalIntegrationTester/RunTests -noHTTP -environment IKS -environmentTags env -project /root/StockTrader/StockTrader.ghp -run Suite 
   sleep 10
