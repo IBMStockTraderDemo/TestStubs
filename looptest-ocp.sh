3bef941eb62175563ac3d7b2ff4973ba0875ec00
mikekaczmarski <kacz@Michaels-MacBook.local>
2019-08-05 10:50:24 -0400

    OCP support

---
+++ looptest-ocp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+#  loop running the test scenario until removed
+#
+
+LOOPS=100000
+
+/opt/IBM/RationalTestControlPanel/bin/startup.sh
+
+while [ $LOOPS -gt 0 ]
+do
+  #JWT=$(curl -k -L -H "Authorization: Basic YWRtaW46YWRtaW4=" https://looper-service:9443/looper/jwt)
+
+  #echo "JWT: " $JWT
+
+  #export JWT
+
+  /opt/IBM/RationalIntegrationTester/RunTests -noHTTP -environment IKS -environmentTags env -project /workspace/StockTrader/StockTrader.ghp -run Suite 
+  sleep 10
+done
